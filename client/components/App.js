5eb1c5c5cb3dd46042655b9f134dc776b3b1005b
xiaojun <mengxj08@gmail.com>
2016-11-03 00:45:28 +0800

    Adding Hot reload feature

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default () => {
-    return (
-        <h1>Hello World from react</h1>
-    );
+class App extends React.Component {
+    render(){
+        return (
+            <h1>Hello World from react222</h1>
+        );
+    }
 }
+
+export default App;
