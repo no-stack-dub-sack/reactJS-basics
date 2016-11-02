9778f9111e7b4eabca965885838a32073b3f4ef5
xiaojun <mengxj08@gmail.com>
2016-11-03 00:48:17 +0800

    Adding Hot reload feature

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
     render(){
         return (
-            <h1>Hello World from react222</h1>
+            <h1>Hello World from react - hot reload</h1>
         );
     }
 }
