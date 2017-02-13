57619122b4de45c189fd1f90fa4a336e7226a579
Peter Weinberg <pweinberg633@gmail.com>
2017-02-12 22:12:02 -0500

    adds user to database

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import validateInput from '../shared/validations/signup.js';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user';
 
 let router = express.Router();
 
@@ -7,7 +10,15 @@ router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body);
   
   if(isValid) {
-    res.json({ success: true });
+    const { username, email, password, timezone } = req.body;
+    const password_digest = bcrypt.hashSync(password, 10);
+    
+    User.forge({
+      username, email, password_digest, timezone 
+    }, { hasTimestamps: true }).save()
+      .then(user => res.json({ success: true }))
+      .catch(err => res.status(500).json({ error: err }))
+    
   } else {
     res.status(400).json(errors);
   }
